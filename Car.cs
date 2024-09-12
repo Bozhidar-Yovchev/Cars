0470074cc87983a407432ea02df34907f516888a
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-13 00:41:20 +0300

    Cars

---
+++ Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public Car()
+        {
+            this.Make = "Subaru";
+            this.Model = "Impreza";
+            this.Year = 2005;
+            this.fuelQuantity = 50;
+            this.FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double FuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {this.Make}" + Environment.NewLine +
+                $"Model:{this.Model}"+ Environment.NewLine +
+                $"Year:{this.Year}" + Environment.NewLine +
+                $"fuelQuantity{this.FuelQuantity}"+ Environment.NewLine +
+               $"fuelConsumption{this.FuelConsumption}";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double FuelQuantity = double.Parse(Console.ReadLine());
+            double FuelConsumption = double.Parse(Console.ReadLine());
+
+
+            Car firstCar = new Car();
+            Console.WriteLine(firstCar);
+
+            Car secondCar = new Car(make, model, year);
+            Console.WriteLine(secondCar);
+
+            Car thridCar = new Car(make, model, year, FuelQuantity, FuelConsumption);
+            Console.WriteLine(thridCar);
+        }
+    }
+}
